4a6c67407be4dc061cdca605513567cee190d74a
Norris Mei <norrismei3@gmail.com>
2020-08-03 17:57:50 -0700

    Completed React jsx file

--- static/js/tradingCard.jsx
+++ static/js/tradingCard.jsx
@@ -29,9 +29,23 @@ function TradingCard(props) {
 }
 
 function TradingCardContainer() {
+  const floatCard = {
+    name: 'Float',
+    skill: 'baking pretzels',
+    imgUrl: '/static/img/float.jpg'
+  };
+
+  const [cards, updateCards] = React.useState([floatCard]);
+
+  React.useEffect(() => {
+    fetch('/cards.json')
+    .then((response) => response.json())
+    .then((data) => updateCards(data))
+  }, [])
+
   const tradingCards = [];
 
-  for (const currentCard of tradingCardData) {
+  for (const currentCard of cards) {
     tradingCards.push(
       <TradingCard
         key={currentCard.name}
